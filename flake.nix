a74dd02a81dff342887cd6f1eb20c91431eb1d02
Vakiyama <vakiyama@outlook.com>
2026-01-13 04:20:57 -0800

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "flake for rust projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+    in
+    {
+      devShell = builtins.listToAttrs (map
+        (system: {
+          name = system;
+          value =
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+            in
+              pkgs.mkShell {
+                buildInputs = with pkgs; [
+                  rustc
+                  cargo
+                  clippy
+                  rust-analyzer
+                  rustfmt
+                ];
+
+
+                shellHook = ''
+                  export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+                '';
+              };
+        })
+        systems);
+    };
+}
